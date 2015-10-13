4bc60ecf8f0ff75199eb10ae07a13e9bdaf6db66
way2muchnoise <jens.baetens@live.be>
2015-10-13 20:00:46 +0200

    adds extension finder script

--- findWhereExtension.py
+++ findWhereExtension.py
@@ -2,15 +2,15 @@ __author__ = 'jens'
 
 import util
 
-# Remove all double entries and entries that have an extension out of the second list
+# Remove all double entries and entries that don't have a given extension out of the second list
 
 print 'Base set'
 base = util.create_set(util.raw_multi_line_input(), lambda s: s.lower())
 print 'Extensions to remove from base'
-to_del = util.create_set(util.raw_multi_line_input(), lambda s: s.lower())
-for to_del_entry in to_del:
+to_find = util.create_set(util.raw_multi_line_input(), lambda s: s.lower())
+for to_find_entry in to_find:
     base_set = set(base)
     for entry in base_set:
-        if entry.endswith(to_del_entry):
+        if not entry.endswith(to_find_entry):
             base.remove(entry)
 util.copy_to_clipboard(sorted(base, key=str.lower))
