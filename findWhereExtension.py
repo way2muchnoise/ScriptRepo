f925a442087abc4a7d06978ca1d2d802aa365ec0
way2muchnoise <jens.baetens@live.be>
2015-10-13 20:14:28 +0200

    fixes find extension script

--- findWhereExtension.py
+++ findWhereExtension.py
@@ -6,11 +6,11 @@ import util
 
 print 'Base set'
 base = util.create_set(util.raw_multi_line_input(), lambda s: s.lower())
-print 'Extensions to remove from base'
+print 'Extensions to find in base'
 to_find = util.create_set(util.raw_multi_line_input(), lambda s: s.lower())
+found = []
 for to_find_entry in to_find:
-    base_set = set(base)
-    for entry in base_set:
-        if not entry.endswith(to_find_entry):
-            base.remove(entry)
-util.copy_to_clipboard(sorted(base, key=str.lower))
+    for entry in base:
+        if entry.endswith(to_find_entry):
+            found.append(entry)
+util.copy_to_clipboard(sorted(found, key=str.lower))
