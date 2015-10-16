ebb993658f226fa2038155eb0dfe5b89e980395e
way2muchnoise <jens.baetens@live.be>
2015-10-16 20:32:19 +0200

    use ' ' instead of '\s'

--- gsmExtract.py
+++ gsmExtract.py
@@ -9,6 +9,6 @@ lines = util.raw_multi_line_input()
 pattern = "(?:\+32|0032|04|0)(\d{8})"
 phones = []
 for line in lines:
-    phones.extend(re.findall(pattern, line.translate(None, '/\s.')))
+    phones.extend(re.findall(pattern, line.translate(None, '/. ')))
 phones = ["0032" + phone for phone in phones]
 util.copy_to_clipboard(phones)
