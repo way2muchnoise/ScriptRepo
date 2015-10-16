c8f7a81d8b87635087129856d4cfc33720142346
way2muchnoise <jens.baetens@live.be>
2015-10-16 20:21:32 +0200

    makes a working phoneExtract
    the previous was just bad

--- gsmExtract.py
+++ gsmExtract.py
@@ -1,19 +1,14 @@
 __author__ = 'jens'
 
 import util
+import re
 
 # Extracts belgian phone numbers from text
 
 lines = util.raw_multi_line_input()
+pattern = "(?:\+32|0032|04|0)(\d{8})"
 phones = []
 for line in lines:
-    splitted = line.split(';')
-    if len(splitted) < 4:
-        continue
-    phone = splitted[3].translate(None, ' /.+')
-    if len(phone) is 10:
-        phone = '0032' + phone[1:]
-    elif len(phone) is 11:
-        phone = '00' + phone
-    phones.append(phone)
+    phones.extend(re.findall(pattern, line.translate(None, '/\s.')))
+phones = ["0032" + phone for phone in phones]
 util.copy_to_clipboard(phones)
