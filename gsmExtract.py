bab4ab4a7beeb0a196e30a234446194e0a616fca
way2muchnoise <jens.baetens@live.be>
2015-10-18 19:59:03 +0200

    fixes for cellphone numbers

--- gsmExtract.py
+++ gsmExtract.py
@@ -6,7 +6,7 @@ import re
 # Extracts belgian phone numbers from text
 
 lines = util.raw_multi_line_input()
-pattern = "(?:\+32|0032|04|0)(\d{8})"
+pattern = "(?:\+32|0032|0)(\d{8}\d?)"
 phones = []
 for line in lines:
     phones.extend(re.findall(pattern, line.translate(None, '/. ')))
